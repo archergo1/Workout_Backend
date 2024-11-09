8c1c58b5a4ec85a450dd1160397dbf13a4746717
archer <econarcher2010@gmail.com>
2024-11-09 16:42:36 +0800

    add models and schemas

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Workout = require("../models/workoutModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,7 +10,17 @@ router.get("/:id", (req, res) => {
   res.json({ message: "GET A SINGLE WORKOUT" });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    // 利用 Workout 這個model 來創建一筆新的資料
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ Error: err.message });
+  }
+
   res.json({ message: "POST A SINGLE WORKOUT" });
 });
 
