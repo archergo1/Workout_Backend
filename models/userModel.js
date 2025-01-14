5b82c26b7d62f1c0aa021607e4bb0fcea28a3dc6
archer <econarcher2010@gmail.com>
2025-01-14 14:39:35 +0800

    Hashing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -13,5 +13,20 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  // 要注意不要在箭頭函式內使用this
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  console.log("salt", salt);
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
