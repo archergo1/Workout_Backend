606adf03f2a30b2c792bd736827d06e222795d2a
archer <econarcher2010@gmail.com>
2025-01-14 12:01:54 +0800

    add User Routes, Controller & Model

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 var cors = require("cors");
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cors());
 
 // routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
