17fcea00a5d6e5c03976845417b6ca0b9f90f87d
archer <econarcher2010@gmail.com>
2025-01-15 14:18:35 +0800

    Logging Users In

--- models/userModel.js
+++ models/userModel.js
@@ -45,4 +45,24 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+  // 比對輸入的密碼和在資料庫中經過雜湊的密碼
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
